13821884c91f54083867bee4a82caf86fe2a8aae
dr-matt-smith <matt.smith@itb.ie>
2020-07-10 17:49:15 +0100

    added scripts for chapter 4

---
+++ Chapter04_audio/04_07_sheduledSound/_Scripts/ButtonActions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ButtonActions : MonoBehaviour 
+{
+    public Text clockText;
+    public Text hoursText;
+    public Text minutesText;
+    public Text secondsText;
+
+    private ScheduledSoundManager _scheduledSoundManager;
+
+    private void Awake()
+    {
+        _scheduledSoundManager = GetComponent<ScheduledSoundManager>();
+    }
+
+    public void ACTION_PlayMusic()
+    {
+        int hours = int.Parse(hoursText.text);
+        int minutes = int.Parse(minutesText.text);
+        int seconds = int.Parse(secondsText.text);
+
+        _scheduledSoundManager.PlayMusic(hours, minutes, seconds);
+    }
+
+    private void Update()
+    {
+        clockText.text = "Time = " + DateTime.Now.ToString("HH:mm:ss");
+    }
+}
---
+++ Chapter04_audio/04_07_sheduledSound/_Scripts/ScheduledSoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ScheduledSoundManager : MonoBehaviour 
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// play sound at specified time
+    /// </summary>
+    public void PlayMusic(int hours, int minutes, int seconds)
+    {
+        DateTime time1147 = DateTime.Today.Add(new TimeSpan(hours, minutes, seconds));
+        TimeSpan delayTimeSpan = time1147 - DateTime.Now;
+        float delayMilliseconds = delayTimeSpan.Seconds;
+
+        print("delay milliseconds = " + delayMilliseconds);
+
+        audioSource.PlayDelayed(delayMilliseconds);
+    }
+}
