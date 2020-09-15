b9714fa19e594f96addb0ed1b0db1d780ce6aa8b
dr-matt-smith <matt.smith@itb.ie>
2020-09-15 19:06:39 +0100

    finished chapter 11 recipes

---
+++ Chapter11_cameras_rendering/11_03_render_partial_screen/_Scripts/GameTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour
+{
+    private Text _textUITime;
+    private float _startTime;
+    
+    void Awake()
+    {
+        _textUITime = GetComponent<Text>();
+        _startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float elapsedSeconds = (Time.time - _startTime);
+        
+        // 1 decimal place
+        string timeMessage = "Elapsed time = " + elapsedSeconds.ToString ("F");
+        _textUITime.text = timeMessage;
+    }
+}
---
+++ Chapter11_cameras_rendering/11_04_capture_camera_image/_Scripts/Snapshot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+
+public class Snapshot : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            print("taking snapshot");
+            string timeStamp = DateTime.Now.ToString("HH_mm_ss");
+            ScreenCapture.CaptureScreenshot("snapshot_" + timeStamp + ".png");            
+        }
+    }
+}
