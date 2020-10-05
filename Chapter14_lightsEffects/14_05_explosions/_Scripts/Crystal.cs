18947bf0dfec256851cf24e450e6638fee641cf3
dr-matt-smith <matt.smith@itb.ie>
2020-10-05 14:25:52 +0100

    assets for ch14

---
+++ Chapter14_lightsEffects/14_05_explosions/_Scripts/Crystal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // create explosion at same location as this Crystal
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        
+        // destroy particle system after 1 second
+        Destroy(explosion, 1);
+        
+        // remove this Crystal
+        Destroy(this.gameObject);
+    }
+}
