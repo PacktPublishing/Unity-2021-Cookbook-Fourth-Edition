f3e2741b5b603bc6a1edc0c36f58bc78ca21360b
dr-matt-smith <matt.smith@itb.ie>
2020-10-05 18:41:49 +0100

    ch14 bomb

---
+++ Chapter14_lightsEffects/14_06_delayedBombExplosion/_Scripts/BombBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BombBehaviour : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    void Start()
+    {
+        // after 3 seconds instantiate the explosion
+        float delay = 3;
+        Invoke(nameof(Explode), delay);
+
+    }
+
+    private void Explode()
+    {        
+        // create explosion at same location as this player
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        
+        // destroy particle system after 1 second
+        Destroy(explosion, 1);
+        
+        // --- do any collision logic for bomb here -----
+        //  Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+
+        
+        // destroy this bomb GameObject
+        Destroy(gameObject);
+    }
+}
---
+++ Chapter14_lightsEffects/14_06_delayedBombExplosion/_Scripts/BombFeature.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BombFeature : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            // create bomb at same location as this player
+            Instantiate(bombPrefab, transform.position, transform.rotation);            
+        }
+    }
+    
+}
