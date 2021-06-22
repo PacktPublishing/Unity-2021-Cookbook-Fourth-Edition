bfae36f3c3128ece21bdcac5796cff9796d00a0f
Matt Smith <dr_matt_smith@me.com>
2021-06-22 09:42:58 +0100

    Update EditorGridGizmoMenuItem.cs

--- Chapter13_advancedGizmosTest/13_02_gizmoGrid/_Scripts/Editor/EditorGridGizmoMenuItem.cs
+++ Chapter13_advancedGizmosTest/13_02_gizmoGrid/_Scripts/Editor/EditorGridGizmoMenuItem.cs
@@ -1,16 +1,18 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
 public class EditorGridGizmoMenuItem : Editor
 {
+	const string GRID_GAME_OBJECT_NAME = "___snap-to-grid___";
+	
 	/**
 	 * menu item to create a GridSnapper
 	 */
 	[MenuItem("GameObject/Create New Snapgrid", false, 10000)]
 	static void CreateCustomEmptyGameObject(MenuCommand menuCommand)
 	{
-		GameObject gameObject = new GameObject("___snap-to-grid___");
+		GameObject gameObject = new GameObject(GRID_GAME_OBJECT_NAME);
 		
 		// ensure not a child of any other object
 		gameObject.transform.parent = null;
